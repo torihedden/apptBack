f1c3e4813d3cd77133fc4da9f47592a7bacc3b1a
Tori Hedden <tori.hedden@gmail.com>
2018-05-18 21:13:51 -0400

    created routes

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
+
+MongoClient.connect('mongodb://localhost:27017', function (err, client) {
+  if (err) {
+    console.log(err);
+  }
+  let db = client.db('appointmentApp');
+
+  app.use(express.json());
+  app.use(express.urlencoded({extended: true}));
+
+  app.get('/appointments', function (request, response) {
+    db.collection('appointments').find({})
+      .toArray((error, appointments) => {
+        response.send(appointments);
+      })
+  });
+
+  app.get('/appointments/search/:param', function (request, response) {
+    let param = request.params.param;
+    db.collection('appointments').find({ description: new RegExp(param, "i") })
+      .toArray((error, appointments) => {
+        response.send(appointments);
+      })
+  });
+
+  app.post('/appointments/create', function (request, response) {
+    const {dateTime, description} = request.body;
+    db.collection('appointments').insertOne({dateTime: dateTime, description: description}, function (error, insertedDocument) {
+      if (error) {
+        response.send('Error occured when creating appointment.');
+      } else {
+        response.send(insertedDocument.ops[0]);
+      }
+    })
+  });
+
+  const port = 8085;
+  app.listen(port);
+  console.log('listening on port' + port);
+})
